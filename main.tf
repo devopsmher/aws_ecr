31683cb96fa9c26cafca836eabf7de59f964bf6c
devopsmher <87768341+devopsmher@users.noreply.github.com>
2021-07-23 00:30:43 +0400

    delete access_key  secret_key

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-provider "aws" {
-    access_key = "AKIA2BFLQL7NRVGDLLTM"
-    secret_key = "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"
-    region = "us-east-2"
-}
-
-
-
 module "ecr" {
   source = "lgallard/ecr/aws"
   for_each = { for x in ["ansible", "jankins", "nginx"] : x => x }
