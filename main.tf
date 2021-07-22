58eb300ad63a85a03b85184907a778bfa3783e68
devopsmher <87768341+devopsmher@users.noreply.github.com>
2021-07-23 03:32:49 +0400

    change main tf
    added provider

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
+provider "aws" {
+    access_key = ""
+    secret_key = ""
+    profile = "default"
+    region = "us-east-2"
+}
+
 module "ecr" {
   source  = "cloudposse/ecr/aws"
   version = "0.32.2"
   for_each = { for x in var.repo : x => x }
   name  = "${each.value}"
 }
+
